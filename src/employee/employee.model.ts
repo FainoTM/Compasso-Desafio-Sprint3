587328bafbdd508eac12e6ae730d582d15fe31b3
Theo Faino <theomfaino@gmail.com>
2022-07-30 12:56:12 -0400

    employee model now using class-validator

--- src/employee/employee.model.ts
+++ src/employee/employee.model.ts
@@ -1,18 +1,39 @@
 import { office } from "src/enum/enumoffice";
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class Employee{
     id: number;
+
+
+    @IsNotEmpty({
+        message: 'Nome do empregado é obrigatório'
+    })
+    @IsString({
+        message: 'Nome inválido'
+    })
     name: string;
+    
+    @IsNotEmpty({
+        message: 'cpf é obrigatório'
+    })
+    @IsNumber({}, {
+        message: 'cpf Inválido'
+    })
     cpf: number;
+    
+    @IsNotEmpty({
+        message: 'cargo do funcionário é obrigatório'
+    })
     office: office;
+    
+    @IsNotEmpty({
+        message: 'Data de nascimento é obrigatório'
+    })
+    @IsDate({
+        message: 'Data invalida'
+    })
     birthday: Date;
 
-    constructor(name: string, cpf: number, office: office, birthday: Date){
-        this.name = name;
-        this.cpf = cpf;
-        this.office = office;
-        this.birthday = birthday;
-    }
 };
 
 

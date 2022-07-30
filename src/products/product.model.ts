0d3813ebf56bb91d41682a3c98f8057736661221
Theo Faino <theomfaino@gmail.com>
2022-07-30 12:14:33 -0400

    created product model

---
+++ src/products/product.model.ts
@@ -0,0 +1,13 @@
+export class Product{
+    id: number;
+    name: string;
+    category: string;
+    price: number;
+    
+
+    constructor(name: string, category: string, price: number, ){
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+};

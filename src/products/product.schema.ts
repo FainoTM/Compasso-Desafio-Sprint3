41e7dcaecbfa373fcf4ae6e7260ad0d86dc1bbaf
Theo Faino <theomfaino@gmail.com>
2022-07-30 12:21:17 -0400

    creating product schema

---
+++ src/products/product.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type productDocument = product & Document;
+
+@Schema()
+export class product {
+
+  @Prop({required: true})
+  name: string;
+
+  @Prop({required: true})
+  category: string;
+
+  @Prop({required: true})
+  price: number;
+
+}
+
+export const ProductSchema = SchemaFactory.createForClass(product);
